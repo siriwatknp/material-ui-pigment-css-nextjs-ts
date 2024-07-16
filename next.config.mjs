8dc1a2dd4e8d6b41ef9b2384505ec7da81721c58
siriwatknp <siriwatkunaporn@gmail.com>
2024-07-16 18:08:51 +0700

    add homepage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import { withPigment } from "@pigment-css/nextjs-plugin";
+import { extendTheme } from "@mui/material";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPigment(nextConfig, {
+  theme: extendTheme({
+    typography: {
+      fontFamily: "var(--font-family)",
+    },
+  }),
+  transformLibraries: ["@mui/material"],
+});
